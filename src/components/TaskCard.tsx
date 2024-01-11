71339eb9795163ccd8906e1d1f5f6b9cc8e3ad0c
kiibler <aleksi.poikolainen@gmail.com>
2024-01-11 18:11:37 +0200

    tweaked UI for mobile view

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -16,9 +16,9 @@ const TaskCard = ({ task }: Props) => {
         <div
             className={`${
                 done
-                    ? "border-green-600 shadow-green-300 sm:border-green-300 sm:hover:border-green-600 sm:hover:shadow-green-300"
-                    : "border-red-600 shadow-red-300 sm:border-red-300 sm:hover:border-red-600 sm:hover:shadow-red-300"
-            } flex break-all rounded-lg border-2 py-2 shadow-lg sm:break-keep sm:hover:shadow-lg md:py-8`}
+                    ? "border-green-600 shadow-green-300 sm:border-green-300 sm:hover:border-green-600"
+                    : "border-red-600 shadow-red-300 sm:border-red-300 sm:hover:border-red-600"
+            } flex break-all rounded-lg border-2 py-2 shadow-lg sm:break-keep md:py-8`}
         >
             <input
                 type="checkbox"
