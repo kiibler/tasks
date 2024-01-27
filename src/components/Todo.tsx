2b7e4a91029329fdf066068e8b8c96c228cf83b9
kiibler <aleksi.poikolainen@gmail.com>
2024-01-27 23:00:56 +0200

    feat: added confirmation on todo deletion

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,15 +1,17 @@
 import { Todos } from "@prisma/client";
 import { MdDeleteOutline } from "react-icons/md";
-import { HTMLAttributes, useOptimistic, startTransition } from "react";
-
+import { useRef, useOptimistic, startTransition } from "react";
 import { deleteTodo, toggleTodoState } from "@/lib/todoActions";
 
-interface Props extends HTMLAttributes<HTMLDivElement> {
+import Dialog from "./Dialog";
+
+interface Props {
     todo: Todos;
 }
 
 const Todo = ({ todo }: Props) => {
     const [optimisticState, addOptimistic] = useOptimistic(todo.finished);
+    const dialogRef = useRef<HTMLDialogElement>(null);
 
     return (
         <div className="flex items-center justify-between border-t p-2 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:last:rounded-b-xl dark:hover:bg-gray-900">
@@ -34,10 +36,22 @@ const Todo = ({ todo }: Props) => {
                 </p>
                 <button
                     className="h-fit rounded-lg border bg-gray-100 p-1 hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-900 dark:hover:bg-gray-800"
-                    onClick={() => deleteTodo(todo.id)}
+                    onClick={() => dialogRef.current?.showModal()}
                 >
                     <MdDeleteOutline />
                 </button>
+                <Dialog dialogRef={dialogRef}>
+                    <p>Toimintoa ei voi perua</p>
+                    <button
+                        className="rounded-lg bg-red-700 p-2 hover:bg-red-600"
+                        onClick={() => {
+                            deleteTodo(todo.id);
+                            dialogRef.current?.close();
+                        }}
+                    >
+                        Poista
+                    </button>
+                </Dialog>
             </div>
         </div>
     );
